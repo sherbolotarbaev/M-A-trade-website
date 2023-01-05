88de9df89ec741040ea148c32a558ead79307e8c
Sherbo1ot <arbaevsherbolot1@gmail.com>
2023-01-06 04:33:36 +0600

    add search

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ hamburger.onclick = function () {
   navBar = document.querySelector(".navbar");
   navBar.classList.toggle("active");
 };
+
+const icon = document.querySelector(".icon");
+const search = document.querySelector(".search");
+const clear = document.querySelector(".clear");
+
+icon.onclick = function () {
+  search.classList.toggle("active");
+};
+
+clear.onclick = function () {
+  document.getElementById("searchId").value = "";
+};
