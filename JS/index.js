2e489b564a3f3eb00084d24076ef4d69594acf07
Sherbo1ot <arbaevsherbolot1@gmail.com>
2023-01-13 01:24:32 +0600

    fixed bugs

--- JS/index.js
+++ JS/index.js
@@ -1,50 +1,107 @@
-// burger menu
-hamburger = document.querySelector(".hamburger");
+// burger menu твой OLD
+// hamburger = document.querySelector(".hamburger");
+//
+// hamburger.onclick = function () {
+// 	navBar = document.querySelector(".navbar");
+// 	navBar.classList.toggle("active");
+// };
+//
+// $(document).ready(function () {
+// 	$("#nav-icon3").click(function () {
+// 		$(this).toggleClass("open");
+// 	});
+// });
+//
+// $(document).ready(function () {
+// 	let hamburger = $("#nav-icon3");
+// 	let navbar = $(".navbar");
+//
+// 	navbar.find("a").click(function () {
+// 		hamburger.click();
+// 	});
+// });
 
-hamburger.onclick = function () {
-  navBar = document.querySelector(".navbar");
-  navBar.classList.toggle("active");
-};
+// burger menu V2
+// $(document).ready(function () {
+// 	// Выберите элемент кнопки бургера с помощью его ID
+// 	let hamburger = $("#nav-icon");
+// 	// Выберите элемент меню с помощью его класса
+// 	let navbar = $(".navbar");
+//
+// 	// Прикрепите обработчик события клика к кнопке бургера
+// 	hamburger.on("click", function () {
+// 		// Переключить класс "active" у элемента меню
+// 		navbar.toggleClass("active");
+// 		// Переключить класс "open" у элемента кнопки бургера
+// 		hamburger.toggleClass("open");
+// 	});
+//
+// 	// Прикрепите обработчик события клика к ссылкам внутри элемента меню
+// 	navbar.find("a").on("click", function () {
+// 		// Симулировать клик по кнопке бургера
+// 		hamburger.click();
+// 	});
+// });
 
-$(document).ready(function () {
-  $("#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4").click(function () {
-    $(this).toggleClass("open");
-  });
+// burger menu V3
+// Ванильный JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+	// Выбрать элемент кнопки бургера с помощью его ID
+	let hamburger = document.getElementById("nav-icon");
+	// Выбрать элемент меню с помощью его класса
+	let navbar = document.querySelector(".navbar");
+	let navLinks = document.querySelectorAll(".nav-links a");
+
+	// Прикрепить обработчик события клика к кнопке бургера
+	hamburger.addEventListener("click", function () {
+		// Переключить класс "active" у элемента меню
+		navbar.classList.toggle("active");
+		// Переключить класс "open" у элемента кнопки бургера
+		hamburger.classList.toggle("open");
+	});
+
+	//  добавляем обработчик события click к каждой ссылке меню
+	navLinks.forEach((link) => {
+		link.addEventListener("click", function () {
+			navbar.classList.remove("active");
+			hamburger.classList.remove("open");
+		});
+	});
 });
+
 // search
 const icon = document.querySelector(".icon");
 const search = document.querySelector(".search");
 const clear = document.querySelector(".clear");
 
 icon.onclick = function () {
-  search.classList.toggle("active");
+	search.classList.toggle("active");
 };
 
 clear.onclick = function () {
-  document.getElementById("searchId").value = "";
+	document.getElementById("searchId").value = "";
 };
 
 // slick
-
 $(document).ready(function () {
-  $(".slider").slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    fade: true,
-    cssEase: "linear",
+	$(".slider").slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		fade: true,
+		cssEase: "linear",
 
-    responsive: [
-      {
-        breakpoint: 750,
-        settings: {
-          dots: false,
-        },
-      },
-    ],
-  });
+		responsive: [
+			{
+				breakpoint: 750,
+				settings: {
+					dots: false
+				}
+			}
+		]
+	});
 });
