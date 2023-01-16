4a6ea4ad6aa15222337d54a289bee89cadc325ca
Sherbo1ot <arbaevsherbolot1@gmail.com>
2023-01-16 14:33:37 +0600

    add animation

--- JS/index.js
+++ JS/index.js
@@ -46,27 +46,27 @@
 // burger menu V3
 // Ванильный JavaScript
 document.addEventListener("DOMContentLoaded", function () {
-	// Выбрать элемент кнопки бургера с помощью его ID
-	let hamburger = document.getElementById("nav-icon");
-	// Выбрать элемент меню с помощью его класса
-	let navbar = document.querySelector(".navbar");
-	let navLinks = document.querySelectorAll(".nav-links a");
+  // Выбрать элемент кнопки бургера с помощью его ID
+  let hamburger = document.getElementById("nav-icon");
+  // Выбрать элемент меню с помощью его класса
+  let navbar = document.querySelector(".navbar");
+  let navLinks = document.querySelectorAll(".nav-links a");
 
-	// Прикрепить обработчик события клика к кнопке бургера
-	hamburger.addEventListener("click", function () {
-		// Переключить класс "active" у элемента меню
-		navbar.classList.toggle("active");
-		// Переключить класс "open" у элемента кнопки бургера
-		hamburger.classList.toggle("open");
-	});
+  // Прикрепить обработчик события клика к кнопке бургера
+  hamburger.addEventListener("click", function () {
+    // Переключить класс "active" у элемента меню
+    navbar.classList.toggle("active");
+    // Переключить класс "open" у элемента кнопки бургера
+    hamburger.classList.toggle("open");
+  });
 
-	//  добавляем обработчик события click к каждой ссылке меню
-	navLinks.forEach((link) => {
-		link.addEventListener("click", function () {
-			navbar.classList.remove("active");
-			hamburger.classList.remove("open");
-		});
-	});
+  //  добавляем обработчик события click к каждой ссылке меню
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      navbar.classList.remove("active");
+      hamburger.classList.remove("open");
+    });
+  });
 });
 
 // search
@@ -75,33 +75,75 @@ const search = document.querySelector(".search");
 const clear = document.querySelector(".clear");
 
 icon.onclick = function () {
-	search.classList.toggle("active");
+  search.classList.toggle("active");
 };
 
 clear.onclick = function () {
-	document.getElementById("searchId").value = "";
+  document.getElementById("searchId").value = "";
 };
 
 // slick
 $(document).ready(function () {
-	$(".slider").slick({
-		dots: true,
-		arrows: false,
-		infinite: true,
-		slidesToShow: 1,
-		adaptiveHeight: true,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		fade: true,
-		cssEase: "linear",
+  $(".slider").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    cssEase: "linear",
 
-		responsive: [
-			{
-				breakpoint: 750,
-				settings: {
-					dots: false
-				}
-			}
-		]
-	});
+    responsive: [
+      {
+        breakpoint: 750,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
+  });
+});
+
+// Animation
+
+document.addEventListener("DOMContentLoaded", () => {
+  const homePage = document.querySelector(".home-page");
+  const header = document.querySelector(".header");
+  const scrollItems = document.querySelectorAll(".scroll-item");
+
+  const homeImgAnimation = () => {
+    let windowCenter = window.innerHeight / 2 + window.scrollY;
+    console.log(windowCenter);
+
+    scrollItems.forEach((el) => {
+      let scrollOffset = el.offsetTop + el.offsetHeight / 8;
+      if (windowCenter >= scrollOffset) {
+        el.classList.add("animation");
+      } else {
+        el.classList.remove("animation");
+      }
+    });
+  };
+
+  const headerFixed = () => {
+    let scrollTop = window.scrollY;
+    let homePageCenter = homePage.offsetHeight / 2;
+
+    if (scrollTop >= homePageCenter) {
+      header.classList.add("fixed");
+      homePage.style.marginTop = `${header.offsetHeight}px`;
+    } else {
+      header.classList.remove("fixed");
+      homePage.style.marginTop = `0px`;
+    }
+  };
+
+  headerFixed();
+  homeImgAnimation();
+  window.addEventListener("scroll", () => {
+    headerFixed();
+    homeImgAnimation();
+  });
 });
