d240bd6cbd77acebd883df7b21a1ae64ca82e713
Sherbolot <arbaevsherbolot1@gmail.com>
2023-01-24 22:12:46 +0600

    add animation, changhe text

--- JS/index.js
+++ JS/index.js
@@ -130,4 +130,22 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", () => {
     headerFixed();
   });
+
+  function onEntry(entry) {
+    entry.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add("element-show");
+      }
+    });
+  }
+
+  let options = {
+    threshold: [0.5],
+  };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll(".anim");
+
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
 });
