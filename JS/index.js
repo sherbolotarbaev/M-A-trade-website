748337210f9fe170f5c1a3b9cbd698a7770facf6
Sherbolot <arbaevsherbolot1@gmail.com>
2023-02-03 12:05:12 +0600

    change search bar

--- JS/index.js
+++ JS/index.js
@@ -70,13 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // search
-const icon = document.querySelector(".icon");
-const search = document.querySelector(".search");
 const clear = document.querySelector(".clear");
-
-icon.onclick = function () {
-  search.classList.toggle("active");
-};
 
 clear.onclick = function () {
   document.getElementById("searchId").value = "";
